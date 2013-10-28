7deee0e057461e844c685f7595634b7507419ce9
Luc Saffre <luc.saffre@gmx.net>
2013-10-28 21:17:38 +0200

    http://code.google.com/p/lino/source/browse/docs/blog/2013/1028.rst

--- tests/faggio_demo_tests.py
+++ tests/faggio_demo_tests.py
@@ -1,9 +1,24 @@
+# -*- coding: UTF-8 -*-
+## Copyright 2013 Luc Saffre
+## This file is part of the Lino-Faggio project.
+## Lino-Faggio is free software; you can redistribute it and/or modify 
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation; either version 3 of the License, or
+## (at your option) any later version.
+## Lino-Faggio is distributed in the hope that it will be useful, 
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+## GNU General Public License for more details.
+## You should have received a copy of the GNU General Public License
+## along with Lino-Faggio; if not, see <http://www.gnu.org/licenses/>.
+
 """
 
 To run only this test::
 
-  $ export DJANGO_SETTINGS_MODULE=lino_faggio.settings.test
-  $ python -m unittest tests.faggio_demo_test
+  $ go faggio
+  $ python setup.py test -s tests.faggio_demo_tests
+
  
 
 """
