2a0be34d2f372f205aa3a66dd5b7c74a9ad5db80
Luc Saffre <luc.saffre@gmx.net>
2013-11-12 23:08:06 +0200

    http://code.google.com/p/lino/source/browse/docs/blog/2013/1112.rst

--- manage.py
+++ manage.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python
 if __name__ == "__main__":
-
+    import sys
     import os
     os.environ['DJANGO_SETTINGS_MODULE'] = 'lino_faggio.settings.demo'
-
-    #~ from django.conf import settings
-    from lino_faggio.settings import demo
-
-    from django.core.management import execute_manager
-
-    execute_manager(demo)
-    #~ execute_manager(settings)
-
-
-
-
-
-
-
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
