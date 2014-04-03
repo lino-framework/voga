f06c82dde1791421a463d72cc2a7242848c60774
Luc Saffre <luc.saffre@gmail.com>
2014-04-03 07:25:52 +0300

    http://docs.lino-framework.org/blog/2014/0403.html

--- lino_faggio/fixtures/eiche2lino.py
+++ lino_faggio/fixtures/eiche2lino.py
@@ -150,8 +150,8 @@ def objects():
     yield Loader1().objects()
 
     # this will create subscriptions for each user and calendar
-    from lino.modlib.cal.fixtures.demo2 import subscribe_all
-    yield subscribe_all()
+    # from lino.modlib.cal.fixtures.demo2 import subscribe_all
+    # yield subscribe_all()
 
     if False:
         users.User.objects.all().delete()
