77d826163c48d05c22119bc96cc4fa67e16f1abf
Luc Saffre <luc.saffre@gmail.com>
2014-09-13 16:59:48 +0300

    http://docs.lino-framework.org/blog/2014/0913.html

--- lino_faggio/settings/doctests.py
+++ lino_faggio/settings/doctests.py
@@ -1,7 +1,7 @@
 from ..settings.demo import *
 SITE = Site(globals(),  # no_local=True,
             remote_user_header='REMOTE_USER',
-            hidden_languages='de fr')
+            hidden_languages='de et')
 SITE.appy_params.update(raiseOnError=True)
 #~ hidden_languages is for tested docs
 # DEBUG = True
