a0fc30bf615fd784f735b556f3a31ee8973899c2
Luc Saffre <luc.saffre@gmail.com>
2016-08-11 06:31:39 +0300

    http://luc.lino-framework.org/blog/2016/0811.html

--- lino_voga/setup_info.py
+++ lino_voga/setup_info.py
@@ -37,13 +37,14 @@ SETUP_INFO = dict(
 Lino Voga is a `Lino <http://www.lino-framework.org>`__ application
 for managing courses, participants and meeting rooms.
 
+- The central project homepage is http://voga.lino-framework.org
+
 - For *introductions* and *commercial information* about Lino Voga
   please see `www.saffre-rumma.net
   <http://www.saffre-rumma.net/voga/>`__.
 
-- The central project homepage is http://voga.lino-framework.org
-
-- Online demo site at http://voga-demo.lino-framework.org
+- Online demo site at http://roger.lino-framework.org (English, German, French)
+  and  http://vtp2014.lino-framework.org (English, Estonian)
 
 """,
     author='Luc Saffre',
