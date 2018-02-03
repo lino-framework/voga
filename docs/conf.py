cb4082b714138a5cc9532f8e2c250a24be016494
Luc Saffre <luc.saffre@gmail.com>
2018-02-03 05:56:46 +0200

    http://luc.lino-framework.org/blog/2018/0203.html

--- docs/conf.py
+++ docs/conf.py
@@ -7,13 +7,17 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
-from importlib import import_module
+
 intersphinx_mapping = {}
-for n in 'atelier lino_cosi'.split():
-    m = import_module(n)
-    # p = Path(m.__file__).parent().parent()
-    n = n.replace('_', "")
-    intersphinx_mapping[n] = (m.intersphinx_urls['docs'], None)
+# from importlib import import_module
+# for n in 'atelier lino_cosi'.split():
+#     m = import_module(n)
+#     # p = Path(m.__file__).parent().parent()
+#     n = n.replace('_', "")
+#     intersphinx_mapping[n] = (m.intersphinx_urls['docs'], None)
+
+from atelier.sphinxconf import interproject
+interproject.configure(globals(), 'atelier lino_cosi')
 
 extlinks = {}
 extensions = []
