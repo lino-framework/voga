0feb5146e010befe549c3611010264421e82dbad
Luc Saffre <luc.saffre@gmail.com>
2021-04-01 22:14:56 +0300

    https://luc.lino-framework.org/blog/2021/0401.html

--- lino_voga/lib/voga/layouts.py
+++ lino_voga/lib/voga/layouts.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2017 Rumma & Ko Ltd
+# Copyright 2017-2021 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
 """The default :attr:`custom_layouts_module
 <lino.core.site.Site.custom_layouts_module>` for Lino Voga.
@@ -8,10 +8,9 @@
 
 from lino.api import rt
 
-rt.models.system.SiteConfigs.detail_layout = """
+rt.models.system.SiteConfigs.set_detail_layout("""
 site_company next_partner_id:10
 default_build_method simulate_today
 site_calendar default_event_type pupil_guestrole
 max_auto_events hide_events_before
-"""
-
+""", window_size=(60, 'auto'))
